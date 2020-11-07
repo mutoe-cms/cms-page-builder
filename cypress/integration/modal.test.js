45c39c753baa82b6d8e8a53011c2296c927ca589
mutoe <mutoe@foxmail.com>
2020-11-07 22:01:52 +0800

    test: add modal e2e testcase

---
+++ cypress/integration/modal.test.js
@@ -0,0 +1,68 @@
+describe('# Modal', () => {
+  it('should display modal in screen center by default', () => {
+    cy.visit('/')
+    cy.openModal()
+
+    cy.get('.modal')
+      .should('be.visible')
+      .should(([ el ]) => {
+        const style = getComputedStyle(el)
+        const deltaY = Math.abs(parseInt(style.top) - parseInt(style.bottom))
+        expect(deltaY).to.be.at.most(1)
+        const deltaX = Math.abs(parseInt(style.left) - parseInt(style.right))
+        expect(deltaX).to.be.at.most(1)
+      })
+  })
+
+  it('should display the second panel when click the second tab', () => {
+    cy.contains('Second').click()
+    cy.get('.modal').contains('Second panel')
+  })
+
+  it('should can be movable', () => {
+    cy.get('.modal header')
+      .trigger('mousedown')
+      .trigger('mousemove', { movementX: 100, movementY: 100 })
+      .trigger('mouseup', { eventConstructor: 'MouseEvent' })
+
+    cy.get('.modal').should(([ el ]) => {
+      const style = getComputedStyle(el)
+      expect(parseInt(style.bottom)).to.equal(0)
+    })
+  })
+
+  it('should can be resizable', () => {
+    cy.get('.modal .resize-handle')
+      .trigger('mousedown', { force: true })
+      .trigger('mousemove', { movementX: 300, movementY: -300, force: true })
+      .trigger('mouseup', { eventConstructor: 'MouseEvent', force: true })
+
+    cy.get('.modal').should(([ el ]) => {
+      const style = getComputedStyle(el)
+
+      expect(parseInt(style.right)).to.equal(0)
+      expect(parseInt(style.bottom)).to.equal(200)
+    })
+  })
+
+  it('should can be closed', () => {
+    cy.findByRole('button', { name: 'Close modal' }).click()
+    cy.get('.modal').should('not.be.visible')
+  })
+
+  it('should remember last position', () => {
+    cy.openModal(2)
+
+    cy.get('.modal').should(([ el ]) => {
+      const style = getComputedStyle(el)
+
+      expect(parseInt(style.right)).to.equal(0)
+      expect(parseInt(style.bottom)).to.equal(200)
+    })
+  })
+
+  it('should can be closed by ESC keydown', () => {
+    cy.get('body').trigger('keydown', { key: 'Escape' })
+    cy.get('.modal').should('not.be.visible')
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,3 +7,8 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
+
+Cypress.Commands.add('openModal', (nth = 1) => {
+  cy.get(`.section-container > section:nth-child(${nth})`).trigger(`mouseenter`)
+  cy.findByRole('button', { name: 'Settings' }).click()
+})
