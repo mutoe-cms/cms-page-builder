7bb448aae0f127928b189167ab0cef1f39f0e077
mutoe <mutoe@foxmail.com>
2020-11-19 19:55:56 +0800

    fix: modal z-index was covered by other element

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -81,12 +81,7 @@ const onKeyDown = (event: KeyboardEvent) => {
 let modal: HTMLDivElement
 let draggingModal = false
 let resizingModal = false
-const modalStyle: UI.ModalStyle = {
-  width: 0,
-  height: 0,
-  left: 0,
-  top: 0,
-}
+const modalStyle: UI.ModalStyle = {}
 
 onMount(() => {
   const styleInStore = storage.modalPosition
@@ -147,6 +142,7 @@ $border-radius: 4px;
   position: fixed;
   top: calc(50% - 250px);
   left: calc(50% - 200px);
+  z-index: 1000;
   display: flex;
   flex-direction: column;
   align-items: stretch;
