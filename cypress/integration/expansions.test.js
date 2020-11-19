df984cfe3d887b2442651ad6d689e8b05ebe2351
mutoe <mutoe@foxmail.com>
2020-11-19 20:36:38 +0800

    test: add expansions tests

---
+++ cypress/integration/expansions.test.js
@@ -0,0 +1,25 @@
+describe('# Expansions', () => {
+  before(() => {
+    cy.visit('/')
+    cy.openModal()
+  })
+
+  it('should display correct', () => {
+    cy.get('.modal').contains('Text').click()
+
+    cy.get('.expansions .details:first').should('have.class', 'open')
+  })
+
+  it('should collapse other details when click another details', () => {
+    cy.get('.modal').contains('Images').click()
+
+    cy.get('.expansions .details:nth-child(2)').should('have.class', 'open')
+    cy.get('.expansions .details:nth-child(1)').should('not.have.class', 'open')
+  })
+
+  it('should collapse the details when click a detail again', () => {
+    cy.get('.modal').contains('Images').click()
+
+    cy.get('.expansions .details:nth-child(2)').should('not.have.class', 'open')
+  })
+})
